ebf27186166690be6eca4c27c388ca832b24de74
jules1univ <jules.garcia.1@etudiant-univ.rennes1.fr>
2023-12-07 18:15:04 +0100

    add env variable

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
-
 import helmet  from 'helmet';
+import jwt from 'jsonwebtoken';
 import { rateLimit} from 'express-rate-limit';
+import {join} from "path";
 
-
-const jwt = require('jsonwebtoken');
 const sanitizer = require('express-sanitizer');
+require('dotenv').config();
 
 
 const app = express();
@@ -21,10 +21,11 @@ app.use(sanitizer());
 
 app.disable('x-powered-by');
 
-app.get("/", (req, res) => {
-  jwt.
-  
-  res.json({ message: "hello world !" });
+app.use('/', express.static(join(__dirname, "..", "../client/dist"), {index: "index.html"}));
+
+app.get("/api", (req, res) => {
+  const token = jwt.sign({}, process.env.TOKEN_PRIVATE_KEY as jwt.Secret);
+  res.json({ message: token });
 });
 
 app.listen(5000);
